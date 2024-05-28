1b0bf6da61473aeb0ad8f8abd68812aa266ad185
thanhdung <ltdung0802@gmail.com>
2024-05-28 17:34:20 +0700

    fix id mongo

--- models/product.js
+++ models/product.js
@@ -1,5 +1,6 @@
-const { ObjectId } = require('mongodb');
 const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const ObjectId = Schema.ObjectId;
 
 const userSchema = new mongoose.Schema({
     Id: ObjectId,
