49421a17072be4f7f61bc9d0a712e9bf2492ca7a
thanhdung <ltdung0802@gmail.com>
2024-05-29 08:24:22 +0700

    clean code

--- models/product.js
+++ models/product.js
@@ -1,9 +1,7 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
-const ObjectId = Schema.ObjectId;
 
 const userSchema = new mongoose.Schema({
-    Id: ObjectId,
     Name: String,
     Img: String,
     Price: String,
