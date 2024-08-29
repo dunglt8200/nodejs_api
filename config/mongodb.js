e30dfa032b09b2f3ef7cdab6bb7610ffc8af5147
thanhdung <ltdung0802@gmail.com>
2024-08-29 13:46:48 +0700

    add connectionString Com

--- config/mongodb.js
+++ config/mongodb.js
@@ -1,15 +1,21 @@
-const mongoClient = require('mongoose')
+const mongoClient = require('mongoose');
 
 const connectToMongo = () => {
-    const dbName = 'Organic_Food_Shop'
-    mongoClient.connect(`mongodb://127.0.0.1:27017/${dbName}`, {
+    const dbName = "Organic_Food_Shop";
+    const connectionStringCom = "mongodb+srv://dunglth82:Admin%40123@cluster0.w2oedq9.mongodb.net";
+    const connectionStringLocal = "mongodb://127.0.0.1:27017";
+
+    // Determine which connection string to use based on the environment
+    const connectionString = process.env.NODE_ENV === 'development' ? `${connectionStringLocal}/${dbName}` : `${connectionStringCom}/${dbName}`;
+    
+    mongoClient.connect(connectionString, {
         useNewUrlParser: true,
         useUnifiedTopology: true
     })
-        .then(() => console.log('✅ Connected database from mongodb.'))
-        .catch((error) => console.error(`❌ Connect database is failed with error which is ${error}`))
+    .then(() => console.log('Connected to MongoDB'))
+    .catch((error) => console.error(`Failed to connect to MongoDB with error: ${error}`));
 }
 
 module.exports = {
     connectToMongo
-}
+};
